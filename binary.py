086e47f5751e8785b2c6770bcbdf0177cd7f2275
qme1519 <jakubek.mi@gmail.com>
2019-10-11 17:44:53 +0400

    Add files via upload

--- binary.py
+++ binary.py
@@ -7,7 +7,7 @@ def check(number, target=None):
         if value == target or target is None:
             if rest == '':
                 return True
-            else: 
+            else:
                 if check(rest, value):
                     print(part, rest)
 
@@ -15,8 +15,8 @@ def check(number, target=None):
 def binaryDenary(binary):
     binary = str(binary)
     denary = 0
-    for digit in binary:  
-        denary = denary*2 + int(digit)  
+    for digit in binary:
+        denary = denary*2 + int(digit)
     return denary
 
 
@@ -27,8 +27,22 @@ def check2(number):
         if number[i] == '1':
             ones.append(i)
     if len(ones) % 3 != 0:
+        print("not 3")
         return [-1, -1]
     trailingZeros = len(number) - int(ones[-1]) - 1
     part = len(ones) // 3
-    return[ones[part-1] + trailingZeros, ones[2*part - 1] + trailingZeros]
-        
+    i = ones[part - 1] + 1
+    end = i + trailingZeros
+    for i in range(i, end):
+        if number[i] != '0':
+            print("trailing1")
+            return [-1, -1]
+    i = ones[2*part - 1] + 1
+    end = i + trailingZeros
+    for i in range(i, end):
+        if number[i] != '0':
+            print("trailing2")
+            return [-1, -1]
+    return [ones[part-1] + trailingZeros, ones[2*part - 1] + trailingZeros + 1]
+
+check2("01010101001010")
